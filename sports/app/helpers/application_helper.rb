5fee463ff95c61525e2a7d086b44ae7e84685dcc
Matthew Bass <pelargir@gmail.com>
2007-05-01 21:08:45 +0000

    added top menu

--- sports/app/helpers/application_helper.rb
+++ sports/app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def streamlined_top_menus
+    [
+      ["Teams", { :controller => "teams", :action => "index" }],
+      ["Coaches", { :controller => "coaches", :action => "index" }]
+    ]
+  end
 end
