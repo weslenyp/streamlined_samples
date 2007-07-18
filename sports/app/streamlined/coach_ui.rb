68f2bdda01fdc57836fd8420591cb006cd79115c
Matthew Bass <pelargir@gmail.com>
2007-07-18 22:27:17 +0000

    added additional comments to the UI classes

--- sports/app/streamlined/coach_ui.rb
+++ sports/app/streamlined/coach_ui.rb
@@ -1,13 +1,16 @@
-# The Additions module is entirely optional and can be left out of the UI file if it isn't needed.
+# As this example demonstrates, the Additions module is entirely optional and can be left out of
+# the UI file if it isn't needed.
 #
 # module CoachAdditions
 #   def years_of_experience
 #     5
 #   end
 # end
+#
+# Coach.class_eval { include CoachAdditions }
 
 class CoachUI < Streamlined::UI
   # Since we're redirecting back to the list view using render_filter in CoachesController, we need
-  # to disable AJAX when editing and saving a record. If we don't, the UI gets confused.
+  # to disable AJAX when editing and saving a record. The UI gets confused if we don't.
   edit_submit_button :ajax => false
 end
--- sports/app/streamlined/team_ui.rb
+++ sports/app/streamlined/team_ui.rb
@@ -1,11 +1,32 @@
+# This is an example of an Additions module. Additions modules are used to add custom columns to
+# the Streamlined views. The value returned can be a string, raw HTML, etc.
+#
+# Methods in an Additions module have access to all the columns on the current record. For example,
+# we could create an addition that combines the first and last name of a person this way:
+#
+#   module PersonAdditions
+#     def first_and_last_name
+#       "#{first_name} #{last_name}"
+#     end
+#   end
+#
+# Here, we're specifying an additional column called "personnel" that will
+# always display the static value 30.
+#
 module TeamAdditions 
   def personnel
     30
   end
 end
 
-Team.class_eval {include TeamAdditions}
+# Streamlined currently requires that you manually include Additions modules in your model class.
+# This process of inclusion will be automated at some point in the future.
+Team.class_eval { include TeamAdditions }
 
+# The UI class is the heart of Streamlined's declarative system. It enables you to control which
+# columns are displayed in various views, which buttons show up, whether or not columns are linked,
+# and so on. The wiki has a good overview of the declarations available to you.
+#
 class TeamUI < Streamlined::UI
   user_columns  :name, { :html_options => { :class => "team_input_field" }, :link_to => {:action => 'show'}},
                 :city, { :html_options => { :class => "team_input_field", :size => 20 }},
