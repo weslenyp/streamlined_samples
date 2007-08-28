342a780b51f62a16952c28ddd37cb9fe46818efa
Matthew Bass <pelargir@gmail.com>
2007-08-28 12:59:50 +0000

    tweaked render_filters example

--- sports/app/controllers/coaches_controller.rb
+++ sports/app/controllers/coaches_controller.rb
@@ -2,8 +2,8 @@ class CoachesController < ApplicationController
   layout "streamlined"
   acts_as_streamlined
   
-  # Redirects back to the list view instead of the show view when done editing a coach.
+  # Redirects back to the show view instead of the list view when done editing a coach.
   # Note that for this to work, AJAX must be disabled on submit. See the CoachUI class
   # for an example of how this is done.
-  render_filter :update, :success => Proc.new { redirect_to :action => "list" }
+  render_filter :update, :success => Proc.new { redirect_to :action => "show", :id => params[:id] }
 end
