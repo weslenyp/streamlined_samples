a7ed47921f2749ddaa7001467bd1fdb2b9cd637f
Rob Sanheim <rob@thinkrelevance.com>
2008-01-25 16:49:34 +0000

    simple tests

--- acts_as_auth_demo/app/models/user.rb
+++ acts_as_auth_demo/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates_presence_of :username
+  validates_numericality_of :age
 end
--- acts_as_auth_demo/test/unit/user_test.rb
+++ acts_as_auth_demo/test/unit/user_test.rb
@@ -1,8 +1,28 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+describe "a new User" do
+  setup do
+    @user = User.new
+    @user.valid?
   end
+  
+  it "should require username" do
+    @user.errors.on(:username).should.not.be.nil
+  end
+  
+  it "should require age" do
+    @user.errors.on(:username).should.not.be.nil
+  end
+
+  it "should require a numeric age" do
+    @user.age = "foo"
+    @user.errors.on(:age).should.not.be.nil
+  end
+  
+  it "should be valid with age and username" do
+    @user.age = 14
+    @user.username = "john"
+    @user.should.be.valid 
+  end
+  
 end
