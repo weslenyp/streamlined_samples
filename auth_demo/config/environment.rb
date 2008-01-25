f615aaf747c0cdf58ded4986991ebc0b0c61e36b
Rob Sanheim <rob@thinkrelevance.com>
2008-01-25 17:07:52 +0000

    tweak environment

--- auth_demo/config/environment.rb
+++ auth_demo/config/environment.rb
@@ -1,13 +1,4 @@
-# Be sure to restart your server when you modify this file
-
-# Uncomment below to force Rails into production mode when
-# you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
-
-# Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
-
-# Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
@@ -46,14 +37,8 @@ Rails::Initializer.run do |config|
   # (create the session table with 'rake db:sessions:create')
   # config.action_controller.session_store = :active_record_store
 
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
-
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector
-
+  config.active_record.observers = :user_observer # or whatever you 
+  
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
 end
