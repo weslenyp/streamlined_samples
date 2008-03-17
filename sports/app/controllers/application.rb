0379db49542af5b03ccc0e1b2a3b51eeacc431cb
Matthew Bass <pelargir@gmail.com>
2008-03-17 03:36:30 +0000

    Enabled forgery protection for Rails 2

--- sports/app/controllers/application.rb
+++ sports/app/controllers/application.rb
@@ -4,4 +4,10 @@
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_sports_session_id'
+  
+  if Rails::VERSION::MAJOR >= 2
+    # See ActionController::RequestForgeryProtection for details
+    # Uncomment the :secret if you're not using the cookie session store
+    protect_from_forgery # :secret => '8ef71bb3dbb6661eaa92de82e6c1e68d'
+  end
 end
