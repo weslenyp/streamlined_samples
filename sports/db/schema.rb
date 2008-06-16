21633f6bd55f098a9e7104dd8f5cfb35bb020750
Matthew Bass <pelargir@gmail.com>
2008-06-16 21:40:10 +0000

    updated sports app to Rails 2.1

--- sports/db/schema.rb
+++ sports/db/schema.rb
@@ -1,5 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of ActiveRecord to incrementally modify your database, and
+# please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your database schema. If you need
@@ -12,7 +12,7 @@
 ActiveRecord::Schema.define(:version => 10) do
 
   create_table "coaches", :force => true do |t|
-    t.integer "team_id"
+    t.integer "team_id",     :limit => 11
     t.string  "first_name"
     t.string  "last_name"
     t.string  "middle_name"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(:version => 10) do
   end
 
   create_table "fantasy_teams_players", :id => false, :force => true do |t|
-    t.integer "fantasy_team_id"
-    t.integer "player_id"
+    t.integer "fantasy_team_id", :limit => 11
+    t.integer "player_id",       :limit => 11
   end
 
   create_table "home_states", :force => true do |t|
@@ -38,23 +38,23 @@ ActiveRecord::Schema.define(:version => 10) do
   end
 
   create_table "players", :force => true do |t|
-    t.integer "team_id"
+    t.integer "team_id", :limit => 11
     t.string  "name"
   end
 
   create_table "sponsors", :force => true do |t|
     t.string  "name"
     t.string  "spokesperson_type"
-    t.integer "spokesperson_id"
+    t.integer "spokesperson_id",   :limit => 11
   end
 
   create_table "teams", :force => true do |t|
     t.string  "name"
     t.string  "city"
     t.string  "sport"
-    t.integer "ranking"
-    t.integer "home_state_id"
-    t.decimal "annual_sales",  :precision => 8, :scale => 2
+    t.integer "ranking",       :limit => 11
+    t.integer "home_state_id", :limit => 11
+    t.decimal "annual_sales",                :precision => 8, :scale => 2
   end
 
 end
