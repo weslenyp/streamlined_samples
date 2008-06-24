d054abb28b515999c593f5ebd0ce4163b7311db3
Matthew Bass <pelargir@gmail.com>
2008-06-24 01:05:39 +0000

    added examples of edit help text and tooltips to coach_ui.rb

--- sports/app/streamlined/coach_ui.rb
+++ sports/app/streamlined/coach_ui.rb
@@ -10,6 +10,12 @@
 # Coach.class_eval { include CoachAdditions }
 
 Streamlined.ui_for(Coach) do
+  user_columns :first_name, { :html_options => { :title => "This field is required" }},
+               :last_name,
+               :middle_name, { :help => "This field is optional" },
+               :sponsors,
+               :team
+  
   # Since we're redirecting back to the list view using render_filter in CoachesController, we need
   # to disable AJAX when editing and saving a record. The UI gets confused if we don't.
   edit_submit_button :ajax => false
